519e76f166dbd119f6e30d61ec968d6ae2735cc8
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-11-23 12:10:03 -0500

    creates elf class

---
+++ Elf.java
@@ -0,0 +1,16 @@
+
+/**
+ * This is the Elf class
+ * It has 10% chance to do double damage. STR ranges from 5-18 and HP
+ * range from 8-25.
+ *
+ * @author Sahar Naz
+ * @version 2025-10
+ */
+public class Elf extends Creature
+{
+    private static final int MAX_STR = 18;
+    private static final int MIN_STR = 5;
+    private static final int MAX_HP  = 25;
+    private static final int MIN_HP  = 8;
+}
