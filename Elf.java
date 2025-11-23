61d324b466ae50c9a54a5e39df84eb51f1e7984a
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-11-23 12:21:31 -0500

    Update Elf class

--- Elf.java
+++ Elf.java
@@ -13,4 +13,24 @@ public class Elf extends Creature
     private static final int MIN_STR = 5;
     private static final int MAX_HP  = 25;
     private static final int MIN_HP  = 8;
+    
+    public Elf() {
+        super(
+            Randomizer.nextInt(MAX_STR - MIN_STR) + MIN_STR,
+            Randomizer.nextInt(MAX_HP - MIN_HP) + MIN_HP
+        );
+    }
+    
+    //10% chance to double the normal damage 
+    @Override
+    public int attack() {
+        int damage = super.attack();
+
+        // 10% chance → Randomizer.nextInt(10) == 1 (1..10)
+        if (Randomizer.nextInt(10) == 1) {
+            damage *= 2;
+        }
+
+        return damage;
+    }
 }
