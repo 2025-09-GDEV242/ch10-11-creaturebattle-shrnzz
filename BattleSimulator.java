454bcb79ffd5a228dbc3b636936b815085c91f53
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-11-23 13:04:26 -0500

    created battleSimulator class

---
+++ BattleSimulator.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+/**
+ * This is a battle simulator class. It wil build the two armies, 
+ * run the combat loop, handle 1v1 fights and print a battle report 
+ *
+ * @author Sahar Naz
+ * @version 2025-10
+ */
+public class BattleSimulator
+{
+   
+}
